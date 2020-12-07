a9e978d8ffef9fd527285cf7340f4c1e2f060c03
Joel <proxy.coding@gmail.com>
2020-12-07 17:32:05 +1000

    Basic project settings set

---
+++ HazmatEngine/src/Test.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace HazmatEngine {
+	void Print()
+	{
+		printf("Welcome to Hazmat Engine!\n");
+	}
+}
---
+++ HazmatEngine/src/Test.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace HazmatEngine {
+	__declspec(dllexport) void Print();
+}
